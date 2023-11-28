7c0f5a6329c94a091a059eb72c04aaec7e37c26f
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-11-28 14:48:32 -0600

    post route to add a new note

--- routes/routeNotes.js
+++ routes/routeNotes.js
@@ -8,8 +8,23 @@ const {
     writeToFile,
 } = require('../helpers/fsUtils');
 
-// Route to get all of the notes
+// GET route to get all of the notes
 
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST route for adding a new note
+
+notes.post('/', (req, res) => {
+    
+    const { title, text } = req.body;
+    const addNewNote = {
+        title,
+        text,
+        id: uuidv4(),
+    };
+
+    readAndAppend(addNewNote, './db/db.json');
+    res.json('Your new note has been added successfully!');
 });
