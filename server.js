2f108ca929548172b8a891ef28b740631c12dfbe
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-11-28 17:36:38 -0600

    added get routes to server

--- server.js
+++ server.js
@@ -8,5 +8,27 @@ const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
+const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// Homepage GET route 
+
+app.get('/', (req, res) => {
+
+    res.sendFile(path.join(__dirname, './public/index.html'))
+
+});
+
+// Notes page GET route
+
+app.get('/notes', (req, res) => {
+
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+    
+});
 
