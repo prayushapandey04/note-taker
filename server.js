bd901e305a3447ecbb728cfab4364097331e2dcb
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-11-28 17:29:42 -0600

    added server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+// Import the packages
+
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+
+// Setting up port 
+
+const PORT = process.env.PORT || 3001;
+
+
+
