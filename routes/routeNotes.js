80663967074ec71bc03bbd2ff580a86edc826139
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-11-28 14:20:02 -0600

    using fsUtils and separating routes

--- routes/routeNotes.js
+++ routes/routeNotes.js
@@ -1,3 +1,9 @@
 // Establish Router using UUID (also helper functions)
 
 const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+} = require('../helpers/fsUtils');
