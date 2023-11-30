c1658413ebee9fb90d43313f2aaf9c92ad4e645c
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-11-30 15:52:39 -0600

    changes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,17 +1,19 @@
-let noteTitle;
-let noteText;
-let saveNoteBtn;
-let newNoteBtn;
-let noteList;
+if (typeof window !== 'undefined') {
 
-if (typeof window !== 'undefined' && window.location.pathname === '/notes') {
-  noteTitle = document.querySelector('.note-title');
-  noteText = document.querySelector('.note-textarea');
-  saveNoteBtn = document.querySelector('.save-note');
-  newNoteBtn = document.querySelector('.new-note');
-  noteList = document.querySelectorAll('.list-container .list-group');
+  let noteTitle;
+  let noteText;
+  let saveNoteBtn;
+  let newNoteBtn;
+  let noteList;
+
+  if (window.location.pathname === '/notes') {
+    noteTitle = document.querySelector('.note-title');
+    noteText = document.querySelector('.note-textarea');
+    saveNoteBtn = document.querySelector('.save-note');
+    newNoteBtn = document.querySelector('.new-note');
+    noteList = document.querySelectorAll('.list-container .list-group');
+  }
 }
-
 // Show an element
 const show = (elem) => {
   elem.style.display = 'inline';
